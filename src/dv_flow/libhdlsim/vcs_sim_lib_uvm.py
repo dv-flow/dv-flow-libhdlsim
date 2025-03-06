d28a5d7ffa63c0a4ef7be535f7b4b24863395398
Matthew Ballance <matt.ballance@gmail.com>
2025-03-06 23:07:40 +0000

    Add VCS UVM library
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

---
+++ src/dv_flow/libhdlsim/vcs_sim_lib_uvm.py
@@ -0,0 +1,55 @@
+import os
+import asyncio
+import json
+import logging
+import shutil
+from typing import List
+from dv_flow.mgr import TaskDataResult, FileSet
+
+_log = logging.getLogger("SimLibUVM")
+
+async def SimLibUVM(runner, input):
+    ex_memento = input.memento
+
+    # TODO: determine whether we're up-to-date
+
+    with open(os.path.join(input.rundir, 'synopsys_sim.setup'), 'w') as fp:
+        fp.write("%s: %s\n" % (
+            input.params.libname, 
+            os.path.join(input.rundir, 'uvm')))
+        
+    which_vlogan = shutil.which('vlogan')
+    if which_vlogan is None:
+        raise Exception("vlogan not found in PATH")
+
+    vcs_home = os.path.dirname(os.path.dirname(which_vlogan))
+
+    cmd = ['vlogan', '-full64', '-sverilog', '-work', 'uvm', '-ntb_opts', 'uvm-1.2']
+
+    _log.debug("Running vlogan: %s", " ".join(cmd))
+
+    fp = open(os.path.join(input.rundir, 'build.log'), "w")
+    proc = await asyncio.create_subprocess_exec(
+        *cmd,
+        cwd=input.rundir,
+        stdout=fp,
+        stderr=asyncio.subprocess.STDOUT)
+
+    await proc.wait()
+    fp.close()
+
+    if proc.returncode != 0:
+        raise Exception("VCS failed (%d)" % proc.returncode)
+
+    return TaskDataResult(
+        memento=ex_memento,
+        changed=True,
+        output=[
+            FileSet(
+                basedir=os.path.join(vcs_home, "etc/uvm-1.2"),
+                filetype="verilogIncDir"),
+            FileSet(
+                basedir=os.path.join(input.rundir, "uvm"),
+                filetype="simLib")
+        ])
+
