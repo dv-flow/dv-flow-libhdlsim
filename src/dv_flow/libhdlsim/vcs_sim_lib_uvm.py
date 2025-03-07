194cb97fec04d4dea5e87299a8a6c372f0215606
Matthew Ballance <matt.ballance@gmail.com>
2025-03-07 15:20:13 +0000

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/dv_flow/libhdlsim/vcs_sim_lib_uvm.py
+++ src/dv_flow/libhdlsim/vcs_sim_lib_uvm.py
@@ -3,6 +3,7 @@ import asyncio
 import json
 import logging
 import shutil
+import pathlib
 from typing import List
 from dv_flow.mgr import TaskDataResult, FileSet
 
@@ -11,37 +12,42 @@ _log = logging.getLogger("SimLibUVM")
 async def SimLibUVM(runner, input):
     ex_memento = input.memento
 
-    # TODO: determine whether we're up-to-date
-
-    with open(os.path.join(input.rundir, 'synopsys_sim.setup'), 'w') as fp:
-        fp.write("uvm: %s\n" % os.path.join(input.rundir, 'uvm'))
-        
     which_vlogan = shutil.which('vlogan')
     if which_vlogan is None:
         raise Exception("vlogan not found in PATH")
 
     vcs_home = os.path.dirname(os.path.dirname(which_vlogan))
+    changed = False
 
-    cmd = ['vlogan', '-full64', '-sverilog', '-work', 'uvm', '-ntb_opts', 'uvm-1.2']
+    # Determine whether we're up-to-date
+    if not os.path.isfile(os.path.join(input.rundir, 'libuvm.d')):
 
-    _log.debug("Running vlogan: %s", " ".join(cmd))
+        with open(os.path.join(input.rundir, 'synopsys_sim.setup'), 'w') as fp:
+            fp.write("uvm: %s\n" % os.path.join(input.rundir, 'uvm'))
+        
 
-    fp = open(os.path.join(input.rundir, 'build.log'), "w")
-    proc = await asyncio.create_subprocess_exec(
-        *cmd,
-        cwd=input.rundir,
-        stdout=fp,
-        stderr=asyncio.subprocess.STDOUT)
+        cmd = ['vlogan', '-full64', '-sverilog', '-work', 'uvm', '-ntb_opts', 'uvm-1.2']
 
-    await proc.wait()
-    fp.close()
+        _log.debug("Running vlogan: %s", " ".join(cmd))
 
-    if proc.returncode != 0:
-        raise Exception("VCS failed (%d)" % proc.returncode)
+        fp = open(os.path.join(input.rundir, 'build.log'), "w")
+        proc = await asyncio.create_subprocess_exec(
+            *cmd,
+            cwd=input.rundir,
+            stdout=fp,
+            stderr=asyncio.subprocess.STDOUT)
+
+        await proc.wait()
+        fp.close()
+
+        if proc.returncode != 0:
+            raise Exception("VCS failed (%d)" % proc.returncode)
+
+        changed = True
 
     return TaskDataResult(
         memento=ex_memento,
-        changed=True,
+        changed=changed,
         output=[
             FileSet(
                 basedir=os.path.join(vcs_home, "etc/uvm-1.2"),
