3fa1c391a7e741dc4a2a6765f270af47725ea3f4
Matthew Ballance <matt.ballance@gmail.com>
2025-03-28 17:43:31 +0000

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/dv_flow/libhdlsim/vcs_sim_image.py
+++ src/dv_flow/libhdlsim/vcs_sim_image.py
@@ -71,8 +71,9 @@ class SimImageBuilder(VlSimImageBuilder):
         if status == 0:
             cmd = ['vcs', '-full64']
 
-            if len(libs):
-                cmd.extend(['-liblist', "+".join(os.path.basename(l) for l in libs)])
+            # Seems that VCS behaves better with the list in the setup file
+#            if len(libs):
+#                cmd.extend(['-liblist', "+".join(os.path.basename(l) for l in libs)])
 
             if len(input.params.top):
                 cmd.extend(['-top', "+".join(input.params.top)])
