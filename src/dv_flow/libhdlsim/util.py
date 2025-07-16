18a8f4bf90006cd68e678e7b922f6c774a4f622d
Matthew Ballance <matt.ballance@gmail.com>
2025-07-16 19:57:00 +0000

    Handle case where args are a string
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/dv_flow/libhdlsim/util.py
+++ src/dv_flow/libhdlsim/util.py
@@ -5,7 +5,10 @@ from typing import List
 
 def merge_tokenize(input : List[str]) -> List[str]:
     merged = []
-    for elem in input:
-        merged.extend(shlex.split(elem))
+    if type(input) == str:
+        merged.extend(shlex.split(str(input)))
+    else:
+        for elem in input:
+            merged.extend(shlex.split(elem))
     return merged
 
